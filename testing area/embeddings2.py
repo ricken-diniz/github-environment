ff81ec5a19cb4a3ceeead5baa01360422bdbf2ca
Ricken <dinizrickenn@gmail.com>
2025-03-23 19:44:44 -0300

    Upgrade my vector database skills

--- testing area/embeddings2.py
+++ testing area/embeddings2.py
@@ -3,8 +3,8 @@ from sentence_transformers import SentenceTransformer, util
 # Carregar o modelo
 modelo = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Lista de frases
-frases = [
+# Lista de docs
+docs = [
     "O cachorro está brincando no parque.",
     "A lua brilha à noite.",
     "O gato dorme no sofá.",
@@ -13,16 +13,16 @@ frases = [
     "Pizza de carne, de frango e de quatro queijos."
 ]
 
-# Gerar embeddings para todas as frases
-embeddings = modelo.encode(frases, convert_to_tensor=True)
+# Gerar embeddings para todas as docs
+embeddings = modelo.encode(docs, convert_to_tensor=True)
 
 # Frase de busca
 consulta = "Quanto custa o carro?"
 emb_consulta = modelo.encode(consulta, convert_to_tensor=True)
 
-# Calcular similaridades com todas as frases
+# Calcular similaridades com todas as docs
 similaridades = util.pytorch_cos_sim(emb_consulta, embeddings)
 
 # Obter índice da frase mais similar
 indice_mais_similar = similaridades.argmax()
-print(f"Frase mais similar: {frases[indice_mais_similar]}")
+print(f"Frase mais similar: {docs[indice_mais_similar]}")
---
+++ testing area/vectordatabase.py
@@ -0,0 +1,90 @@
+'''
+    ÁREA EM CONSTRUÇÃO
+
+    - Lógica sendo pensada meticulosamente para diminuir os custos
+    com recursos e processamento, visto que a cada troca de requisições
+    o código será processado.
+    
+'''
+
+from pymilvus import MilvusClient, DataType
+from pymilvus.model.hybrid import BGEM3EmbeddingFunction
+embedding_model =  BGEM3EmbeddingFunction(
+    model_name='BAAI/bge-m3',
+    device='cpu',
+    use_fp16=False
+)
+
+# from pymilvus import model
+
+# embedding_fn = embedding_model.DefaultEmbeddingFunction()
+client = MilvusClient("milvus_demo.db") # Client for a local database, this can be made with cluster
+
+
+index_params = client.prepare_index_params()
+
+index_params.add_index(
+    field_name="dense",
+    index_name="dense_index",
+    index_type="IVF_FLAT",
+    metric_type="IP",
+    params={"nlist": 128},
+)
+
+index_params.add_index(
+    field_name="sparse",
+    index_name="sparse_index",
+    index_type="SPARSE_INVERTED_INDEX",
+    metric_type="IP",
+    params={"inverted_index_algo": "DAAT_MAXSCORE"},
+)
+
+def embedding_docs(docs: dict):
+    # Gerar embeddings para todas as docs
+    embeddings = embedding_model.encode_documents(docs)
+    return embeddings
+    # 'embeddings' will be a dict with 'dense' and 'sparse' attributes, e.g.: {'dense': [list], 'sparse': [list]}
+
+
+schema = MilvusClient.create_schema(
+    auto_id=False,
+    enable_dynamic_field=True,
+)
+# client.drop_collection(collection_name="hybrid_search_collection")
+if not client.has_collection(collection_name="hybrid_search_collection"):
+    schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
+    schema.add_field(field_name="text", datatype=DataType.VARCHAR, max_length=1000)
+    schema.add_field(field_name="sparse", datatype=DataType.SPARSE_FLOAT_VECTOR)
+    schema.add_field(field_name="dense", datatype=DataType.FLOAT_VECTOR, dim=250002)
+
+    client.create_collection(
+        collection_name="hybrid_search_collection",
+        schema=schema,
+        index_params=index_params
+    )
+
+
+# Insert more docs in another subject.
+docs = [
+    "Machine learning has been used for drug design.",
+    "Computational synthesis with AI algorithms predicts molecular properties.",
+    "DDR1 is involved in cancers and fibrosis.",
+]
+vectors = embedding_model.encode_documents(docs)
+print(vectors['sparse'][0])
+data = [
+    {"id": 3 + i, "text": docs[i],"sparse": vectors['sparse'][i], "dense":  vectors['dense'][i]}
+    for i in range(len(vectors))
+]
+
+client.insert(collection_name="hybrid_search_collection", data=data)
+
+# This will exclude any text in "history" subject despite close to the query vector.
+res = client.search(
+    collection_name="hybrid_search_collection",
+    data=embedding_model.encode_queries(["tell me AI related information"]),
+    limit=2,
+    output_fields=["text"],
+)
+
+print(res)
